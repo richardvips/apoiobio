e5fc97710ee73c28aa19623c35f027e556a66f55
Richard Tomaz <36301585+richardvips@users.noreply.github.com>
2021-10-03 06:00:49 -0300

    INICIAL
    Projeto inicial

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # apoiobio
+<<<<<<< HEAD
 Site de apoio ao agendamento biolabor 
 O  intuito seria auxiliar  os funcionarios da empresa na qual eu trabalho com agendamenmento de exames. O sistema possui uma função simples de calculos nescessarios e outras funções que achei interessante incluir. 
+=======
+Site de apoio ao agendamento biolabor
+>>>>>>> parent of 0102474 (Sistema bio teste)
---
+++ arq/js/codigo.js
@@ -0,0 +1,62 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("activem");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnfContainer");
+var btns = btnContainer.getElementsByClassName("btnf");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("activem");
+    current[0].className = current[0].className.replace(" activem", "");
+    this.className += " activem";
+  });
+}
